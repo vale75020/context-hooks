21b480a8b621f534b9910731e502248f966ca22d
vale75020 <valentinosuma@hotmail.com>
2019-09-10 17:10:27 +0200

    context provider

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+    render() {
+        return (
+            <div className="booklist">
+                <ul>
+                    <li>Lion King</li>
+                    <li>Mystery</li>
+                    <li>Siddartha</li>
+                </ul>
+            </div>
+        )
+    }
+}
